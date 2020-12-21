0d72c9aff15c7b6a6ed722c672124a970ab4f1ba
Jeppe Hyldgaard Olesen <jeppe@gejststudio.com>
2020-12-21 20:33:33 +0100

    Removed deprecated flair task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 const gulp = require('gulp')
 const del = require('del')
-const spritesmith = require('gulp.spritesmith')
 const plumber = require('gulp-plumber')
 const sass = require('gulp-sass')
 const sasslint = require('gulp-sass-lint')
 const autoprefixer = require('gulp-autoprefixer')
-const merge = require('merge-stream')
 
 const project = {
   styles : './src/styles/**/*.scss',
@@ -30,27 +28,13 @@ gulp.task('styles', function() {
     .pipe(gulp.dest(project.dist))
 })
 
-gulp.task('flairs', function() {
-  const spriteData = gulp.src('./src/flairs/*.png').pipe(spritesmith({
-    imgName: 'flairs.png',
-    cssName: '_user-flairs.scss',
-    cssTemplate: './src/flair-template.css.handlebars'
-  }))
-
-  const imageStream = spriteData.img.pipe(gulp.dest('./src/images/'))
-
-  const cssStream = spriteData.css.pipe(gulp.dest('./src/styles/modules/'))
-
-  return merge(imageStream, cssStream)
-})
-
 gulp.task('images', function() {
   return gulp.src(project.images, {base: 'src'})
     .pipe(plumber())
     .pipe(gulp.dest(project.dist))
 })
 
-gulp.task('build', ['flairs'], function() {
+gulp.task('build', function() {
   gulp.start('styles', 'images')
 })
 
